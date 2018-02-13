2aadc281735c0340e432d147cf9ab3c583b2e637
Bueld <bueld.blad@gmail.com>
2018-02-13 18:06:23 +0100

    13.02.2018 18:06 changes saved

--- 3DTests/src/classes/MovingCamera.java
+++ 3DTests/src/classes/MovingCamera.java
@@ -3,11 +3,13 @@ package classes;
 import java.util.ArrayList;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
+import javafx.event.EventHandler;
+import javafx.geometry.Point3D;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.SceneAntialiasing;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Box;
@@ -16,25 +18,35 @@ import javafx.stage.Stage;
 public class MovingCamera extends Application {
 
 	private PerspectiveCamera camera;
-	
+
 	private ArrayList<Box> blocks = new ArrayList<Box>();
+	private Point3D cPoint = new Point3D(0, 0, 0);
+	private double speed = 6;
 
 	@Override
 	public void init() {
-		
-		Box block1 = new Box();
-		block1.setWidth(50);
-		block1.setDepth(50);
-		block1.setHeight(50);
-		block1.setTranslateX(0);
-		block1.setTranslateY(0);
-		block1.setTranslateZ(0);
-		blocks.add(block1);
-		
+
+		for (int i = 0; i <= 9; i++) {
+			for (int j = 0; j <= 9; j++) {
+				for (int k = 0; k <= 9; k++) {
+					Box b = new Box();
+					b.setDepth(20);
+					b.setWidth(20);
+					b.setHeight(20);
+
+					b.setTranslateX((i * 50) + 20);
+					b.setTranslateY((j * 50) + 20);
+					b.setTranslateZ((k * 50) + 20);
+
+					blocks.add(b);
+				}
+			}
+		}
+
 		camera = new PerspectiveCamera();
-		camera.setTranslateX(0);
-		camera.setTranslateY(0);
-		camera.setTranslateZ(-100);
+		camera.setTranslateX(cPoint.getX());
+		camera.setTranslateY(cPoint.getY());
+		camera.setTranslateZ(cPoint.getZ());
 		camera.setNearClip(0.1);
 		camera.setFarClip(5000.0);
 	}
@@ -45,7 +57,6 @@ public class MovingCamera extends Application {
 		bGroup.getChildren().addAll(blocks);
 
 		StackPane pane = new StackPane();
-		pane.setPadding(new Insets(10, 10, 10, 10));
 		pane.getChildren().addAll(bGroup);
 
 		Scene scene = new Scene(pane, 800, 800, true, SceneAntialiasing.DISABLED);
@@ -53,11 +64,57 @@ public class MovingCamera extends Application {
 		scene.setFill(Color.rgb(20, 4, 40));
 
 		stage.setScene(scene);
+
+		scene.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent e) {
+				switch (e.getText()) {
+				case "w":
+					cPoint = new Point3D(cPoint.getX(), cPoint.getY(), cPoint.getZ() + speed);
+					break;
+				case "s":
+					cPoint = new Point3D(cPoint.getX(), cPoint.getY(), cPoint.getZ() - speed);
+					break;
+				case "a":
+					cPoint = new Point3D(cPoint.getX() - speed, cPoint.getY(), cPoint.getZ());
+					break;
+				case "d":
+					cPoint = new Point3D(cPoint.getX() + speed, cPoint.getY(), cPoint.getZ());
+					break;
+				case "q":
+					rotateCamera(-3.0);
+					break;
+				case "e":
+					rotateCamera(3.0);
+					break;
+
+				}
+
+				camera.setTranslateX(cPoint.getX());
+				camera.setTranslateZ(cPoint.getZ());
+			}
+
+		});
+
 		stage.show();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 
+	}
+
+	public void rotateCamera(double d) {
+		camera.setTranslateX(0);
+		camera.setTranslateY(0);
+		camera.setTranslateZ(0);
+
+		camera.setRotationAxis(new Point3D(0, 1, 0));
+		camera.setRotate(camera.getRotate() + d);
+
+		camera.setTranslateX(cPoint.getX());
+		camera.setTranslateY(cPoint.getY());
+		camera.setTranslateZ(cPoint.getZ());
 	}
 }
